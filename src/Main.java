e1bce6a05d30c294ea12f19adc1d6205fe73417c
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 16:01:10 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[]args){
+
+        //crie um programa declarando uma variável do tipo int chamada idade,outra chamada altura e outra peso,ambas
+        //do tipo double,atribua um valor para todas as váriaveis e exiba o resultado no console.
+
+        int idade;
+        double altura;
+        double peso;
+
+        altura = 1.65;
+        peso = 80.100;
+        idade = 26;
+
+
+        System.out.println(altura);
+        System.out.println(peso);
+        System.out.println(idade);
+    }
+}
